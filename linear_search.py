0af8349d898a47bc87dbbcc3a88863d742e3207f
Sahabuddin Shakil <mdshakil8796@gmail.com>
2021-01-14 00:06:51 +0600

    modified plms

---
+++ linear_search.py
@@ -0,0 +1,10 @@
+# linear search algorithm
+array =[2,4,5,6,3,7,8,20]
+seach_value = 11
+def Linear_Search(arr,value):
+    for i in range(len(arr)):
+        if(arr[i]== value):
+            return i
+    return -1
+
+print(Linear_Search(array,seach_value)   )            
