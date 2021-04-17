fdcd01057c130fd00d65ff756db4db80dd11079a
ms-shakil <mdshakil8796@gmail.com>
2021-04-18 04:32:17 +0600

    about algo

--- binary_search.py
+++ binary_search.py
@@ -1,16 +1,31 @@
 # binary search by python
-array =[1,3,5,6,8,9,10,15,18,19,20,21,22]
-value = 20
-def Binary_Search(array,value):
-    left =0
-    right =len(array)
+def Binary_Search(array, value):
+    left = 0
+    right = len(array)
     while left <= right:
-        mid =(left+right)//2
+        mid = (left+right)//2
         if array[mid] == value:
             return mid
-        elif array[mid] < value :
-            left =mid+1
+        elif array[mid] < value:
+            left = mid+1
         else:
-            right= mid -1
-    return -1               
-print(Binary_Search(array,value))
+            right = mid - 1
+    return -1
+
+
+if __name__ == "__main__":
+    array = [1, 3, 5, 6, 8, 9, 10, 15, 18, 19, 20, 21, 22]
+    value = int(input("Enter a number:"))
+    result = Binary_Search(array, value)
+    if result == -1:
+        if value in array:
+            print(value, "is in list,but function returned -1")
+        else:
+            print(value, "not in list")
+
+    else:
+        if array[result] == value:
+            print(value, "found in correct position.")
+        else:
+            print("Binary search returned", result,
+                  "For", value, "which is incorrect.")
--- linear_search.py
+++ linear_search.py
@@ -1,10 +1,13 @@
 # linear search algorithm
-array =[2,4,5,6,3,7,8,20]
+array = [2, 4, 5, 6, 3, 7, 8, 20]
 seach_value = 5
-def Linear_Search(arr,value):
+
+
+def Linear_Search(arr, value):
     for i in range(len(arr)):
-        if(arr[i]== value):
+        if(arr[i] == value):
             return i
     return -1
 
-print(Linear_Search(array,seach_value)   )            
+
+print(Linear_Search(array, seach_value))
